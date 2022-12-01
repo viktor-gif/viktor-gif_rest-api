15679a56249f9db979f960e7c2ff8313c9a75c26
viktor-gif <klim.victor.ua@gmail.com>
2022-12-01 18:35:30 +0200

    commit after lots of time

--- controller/groops.js
+++ controller/groops.js
@@ -1,15 +1,37 @@
 'use strict'
-// const User = require('../models/user').user
+
+const { save } = require('nconf')
+
+const Groop = require('../models/groops').groop
 // const errorHandler = require('../utils/errorHandler')
 // const successHandler = require('../utils/successHandler')
 // const userErrorHandler = require('../utils/userErrorHandler')
 
 exports.createGroop = async (req, res, next) => {
-  res.status(200).json('Create groop is riched')
+  if (!req.session.userId) {
+    res.status(403).json({
+      message: "Ви не зареєстровані. Ввійдіть, будь ласка, в аккаунт."
+    })
+  } else {
+    const newGroop = new Groop({
+      authorId: req.query.authorId,
+      title: req.query.title,
+      type: req.query.groopType
+    })
+    await newGroop.save()
+    res.status(201).json('Groop created')
+  }
 }
+exports.getGroops = async (req, res, next) => {
+  if (!req.session.userId) {
+    res.status(403).json({
+      message: "Ви не зареєстровані. Ввійдіть, будь ласка, в аккаунт."
+    })
+  } else {
+    const groops = await Groop.find()
+    res.status(200).json(groops)
+  }
+};
 exports.deleteGroop = async(req, res, next) => {
   res.status(200).json('Delete groop is riched')
-};
-exports.getGroops = async (req, res, next) => {
-  res.status(200).json('Get groops is riched')
 };
--- models/groops.js
+++ models/groops.js
@@ -1,6 +1,192 @@
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema
 
+const commentsImgSchema = new Schema({
+    authorId: {
+        ref: 'user',
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    linkToAnotherComment: {
+        type: String,
+        default: null
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    commentText: {
+        type: String,
+        default: null
+    },
+    image: {
+        type: String,
+        default: null
+    },
+    video: {
+        type: String,
+        default: null
+    },
+    audio: {
+        type: String,
+        default: null
+    },
+    likedUsers: [String],
+    likesCount: {
+        type: Number,
+        default: 0
+    },
+})
+
+const imgSchema = new Schema({
+    authorId: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        default: null
+    },
+    isPrivat: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentsImgSchema],
+    likedUsers: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const commentsVideoSchema = new Schema({
+    authorId: {
+        ref: 'user',
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    linkToAnotherComment: {
+        type: String,
+        default: null
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    commentText: {
+        type: String,
+        default: null
+    },
+    image: {
+        type: String,
+        default: null
+    },
+    video: {
+        type: String,
+        default: null
+    },
+    audio: {
+        type: String,
+        default: null
+    },
+    likedUsers: [String],
+    likesCount: {
+        type: Number,
+        default: 0
+    },
+})
+
+const videoSchema = new Schema({
+    authorId: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        default: null
+    },
+    isPrivat: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentsVideoSchema],
+    likedUsers: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const commentsAudioSchema = new Schema({
+    authorId: {
+        ref: 'user',
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    linkToAnotherComment: {
+        type: String,
+        default: null
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    commentText: {
+        type: String,
+        default: null
+    },
+    image: {
+        type: String,
+        default: null
+    },
+    video: {
+        type: String,
+        default: null
+    },
+    audio: {
+        type: String,
+        default: null
+    },
+    likedUsers: [String],
+    likesCount: {
+        type: Number,
+        default: 0
+    },
+})
+
+const audioSchema = new Schema({
+    authorId: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        default: null
+    },
+    isPrivat: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentsAudioSchema],
+    likedUsers: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
 const commentsSchema = new Schema({
     authorId: {
         ref: 'user',
@@ -89,25 +275,20 @@ const groopsSchema = new Schema({
     },
     type: {
         type: String,
-        default: ' public'
+        default: 'public'
     },
 
     followers: [String],
     posts: [groopsPostsSchema],
-    postVideo: {
-        type: String,
-        default: null
-    },
-    postAudio: {
-        type: String,
-        default: null
-    },
+    img: [imgSchema],
+    video: [videoSchema],
+    audio: [audioSchema],
     created: {
         type: Date,
         default: Date.now
     }
 })
 
-exports.groops = mongoose.model('groops', groopsSchema)
+exports.groop = mongoose.model('groops', groopsSchema)
 exports.posts = mongoose.model('groopsPosts', groopsPostsSchema)
 exports.comments = mongoose.model('groopsPostsComments', commentsSchema)
