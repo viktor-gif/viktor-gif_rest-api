00e4737f2a17c686304fc181c22fe23e37c6d2f1
viktor-gif <klim.victor.ua@gmail.com>
2023-04-15 14:04:15 +0300

    function "me" update: added data and resultCode in res.json()

--- controller/auth.js
+++ controller/auth.js
@@ -5,12 +5,16 @@ const successHandler = require('../utils/successHandler')
 const userErrorHandler = require('../utils/userErrorHandler')
 
 exports.me = async (req, res, next) => {
+
   try {
     if (req.session.userId) {
       res.status(200).json({
-        id: req.session.userId,
-        email: req.session.email,
-        login: req.session.login,
+        data: {
+          id: req.session.userId,
+          email: req.session.email,
+          login: req.session.login,
+        },
+        resultCode: 0
       })
     } else {
       userErrorHandler(res, 403, "Вибачте, ви не авторизовані")
@@ -19,36 +23,7 @@ exports.me = async (req, res, next) => {
     errorHandler(res, err)
   }
 }
-// exports.login = async(req, res, next) => {
-//   const candidate = await User.findOne({ email: req.query.email })
 
-//   const email = req.query.email
-//   const login = req.query.login
-//   const password = req.query.password
-
-//   if (candidate) {
-
-//     if (candidate.checkPassword(password)) {
-//       const token = jwt.sign({
-//         email: candidate.email,
-//         login: candidate.login,
-//         userId: candidate._id
-//       }, config.get('jwt'), {expiresIn: 60 * 60})
-//       res.status(200).json({
-//         token: `Bearer ${token}`
-//       })
-//     } else {
-//       res.status(401).json({
-//         message: 'Вибачте, пароль не вірний. Спробуйте ще раз.'
-//       })
-//     }
-
-//   } else {
-//     res.status(404).json({
-//       message: 'Користувача з таким e-mail не існує'
-//     })
-//   }
-// };
 exports.login = async (req, res, next) => {
   const email = req.query.email
   const password = req.query.password
@@ -74,6 +49,7 @@ exports.login = async (req, res, next) => {
     errorHandler(res, err)  
   }
 };
+
 exports.logout = async (req, res, next) => {
   try {
     req.session.destroy()
